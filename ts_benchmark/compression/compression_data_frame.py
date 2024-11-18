568b1e4b97c3fe9558ebf69564bdfda3811ce262
Remy Raes <raes.remy@gmail.com>
2024-11-18 15:42:41 +0100

    feat: use a FLI instance to mutate dataframe

--- ts_benchmark/compression/compression_data_frame.py
+++ ts_benchmark/compression/compression_data_frame.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import ciso8601
+from datetime import datetime
 from fast_linear_interpolation import FastLinearInterpolation
 
 
@@ -18,7 +20,28 @@ class CompressionDataFrame(pd.DataFrame):
     def read_csv(path):
         return CompressionDataFrame(pd.read_csv(path))
 
+    def compress(self, tolerated_error):
+        print(self.head())
+
+        data = [(datetime.timestamp(ciso8601.parse_datetime(t)), v) for t, v in zip(self['date'], self['data'])]
+        self.fli.setError(tolerated_error)
+        for d in data:
+            try:
+                self.fli.add(d[0], d[1])
+            except:
+                # todo: fix
+                print(f"todo: fix failing add for {d}")
+
+        # reattribute values
+        assert self.len() == len(data)
+        self['data'] = [self.fli.read(d[0]) for d in data]
+        print(self.head())
+
 #---- self test code
 if __name__ == "__main__":
     compressed_series = CompressionDataFrame.read_csv('dataset/forecasting/PEMS-BAY.csv')
     print(f"Compressed series length: {compressed_series.len()}")
+
+    # Compress with tolerated error
+    error = 0.5
+    compressed_series.compress(error)
