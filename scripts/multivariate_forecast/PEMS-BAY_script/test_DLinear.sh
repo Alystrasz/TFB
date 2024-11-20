bdbcc40455e9092b11287da330a1b6a8be0df0b0
Remy Raes <contact@remyraes.com>
2024-11-20 17:37:50 +0100

    build: add DLinear MTS test script

---
+++ scripts/multivariate_forecast/PEMS-BAY_script/test_DLinear.sh
@@ -0,0 +1,1 @@
+python ./scripts/run_benchmark.py --config-path "rolling_forecast_config.json" --data-name-list "PEMS-BAY.csv" --strategy-args '{"horizon":96}' --model-name "time_series_library.DLinear" --model-hyper-params '{"d_ff": 512, "d_model": 256, "horizon": 96, "seq_len": 336}' --adapter "transformer_adapter"  --gpus 0  --num-workers 1  --timeout 60000  --save-path "PEMS-BAY/DLinear"
